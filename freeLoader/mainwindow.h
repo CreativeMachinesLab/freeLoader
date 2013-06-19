499216b3c3213e83fe5616b4aaa98e395b85d00c
Ethan <goughmax@gmail.com>
2013-06-19 18:37:26 -0400

    made skeleton of mainwindow

--- freeLoader/mainwindow.h
+++ freeLoader/mainwindow.h
@@ -15,8 +15,32 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+public slots:
+
+    void disableJog(bool disabled);
+    void disableTestSettings(bool disabled);
+    void disableAll(bool disabled);
+    void disableTesting(bool disabled);
+
+    //set at dyna connect
+    void setTestSpeedMin(float min);
+    void setTestSpeedMax(float max);
+
+    void testStarted();
+    void testEnded();
+
+
+
+    // Internal because we didnt do this right
+    void jogSpinUpdated();
+    void jogSliderUpdated();
+    void testSpinUpdated();
+    void testSpinUpdated();
+
 private:
     Ui::MainWindow *ui;
+    float minSpeed_;
+    float maxSpeed_;
 };
 
 #endif // MAINWINDOW_H
