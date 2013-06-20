08b363714eaf00901d5bab25f1968ffc4b34cda9
Ethan <goughmax@gmail.com>
2013-06-19 22:55:03 -0400

    testdata writes a csv

--- freeLoader/testdata.cpp
+++ freeLoader/testdata.cpp
@@ -1,4 +1,7 @@
 #include "testdata.h"
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 TestData::TestData(QString filename, QObject *parent) :
     QObject(parent),filename_(filename)
@@ -33,6 +36,28 @@ void TestData::addData(int time, float mm, float newtons){
 }
 
 bool TestData::writeToFile(){
-    return true;
+
+    QVector <float> thisDataPoint(3); //length 3 -> time, pos, load
+
+    QFile fileToWrite(filename_);
+    if(!(fileToWrite.open(QIODevice::WriteOnly | QIODevice::Text))){
+        qDebug()<<"Failed to open writefile";
+        return false;
+    }else{
+        QTextStream writeStream(&fileToWrite);
+        writeStream<<"freeLoader data file: " <<filename_<<"\n";
+        writeStream<<"Data is time (ms), position (mm), load (N) \n \n";
+
+        for(unsigned int i=0; i<data_.size(); ++i)
+        {
+            thisDataPoint=data_[i];
+            writeStream<<thisDataPoint[0]<<","<<thisDataPoint[1]<<","<<thisDataPoint[2]<<"\n";
+
+        }
+        //write successful
+        fileToWrite.close();
+        return true;
+    }
+
 }
 
